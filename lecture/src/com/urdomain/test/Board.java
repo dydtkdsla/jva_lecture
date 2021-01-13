a0c0cc3d4e898d06c3c844f8a3dbd32e834b3b96
박지연 <@192.168.219.103>
2021-01-13 11:21:54 +0900

    210113test

---
+++ lecture/src/com/urdomain/test/Board.java
@@ -0,0 +1,20 @@
+package com.urdomain.test;
+
+public class Board {
+	String boardTitle = "";
+	String postTilte = "";
+	String userName = "";
+	int uploadDate = 0;
+	int postViews = 0;
+
+	public void intoPost( ) {
+		// 게시글보기
+	}
+	public void search( ) {
+		// 검색
+	}
+	public void write( ) {
+		// 글작성
+	}
+	
+}
---
+++ lecture/src/com/urdomain/test/Notice.java
@@ -0,0 +1,10 @@
+package com.urdomain.test;
+
+public class Notice extends Board {
+	
+	this.boardTitle = boardTitle;
+	this.postTitle = postTitle;
+	this.userName = userName;
+	this.uploadDate = uploadDate;
+	this.postViews = postViews;
+}
