02bf49178bfd7e193c6341e5ec54537e563315f4
박지연 <@192.168.219.103>
2021-01-19 16:17:50 +0900

    210119

--- lecture/src/com/urdomain/ch07/ChildExample.java
+++ lecture/src/com/urdomain/ch07/ChildExample.java
@@ -2,11 +2,17 @@ package com.urdomain.ch07;
 
 public class ChildExample {
 	public static void main(String[] args) {
-		Child child = new Child();
-		
-		Parent parent = child;
+		Parent parent = new Child();
+		parent.field1 = "data1";
 		parent.method1();
 		parent.method2();
-		//parent.method3();
+		/*
+		parent.field2 = "data2";
+		parent.metiod3();
+		 */
+		
+		Child child = (Child) parent;
+		child.field2 = "yyy"; //(가능)
+		child.method3();	//(가능)
 	}
 }
