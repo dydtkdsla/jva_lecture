0f4c6a764c1ac2f38c05c5438b580236e7c228a4
박지연 <@192.168.219.103>
2021-01-19 17:37:40 +0900

    210119pm

--- lecture/src/com/urdomain/ch10/CatchByExceptionKindExample.java
+++ lecture/src/com/urdomain/ch10/CatchByExceptionKindExample.java
@@ -1,24 +1,18 @@
 package com.urdomain.ch10;
 
-public class TryCatchFinallyRuntimeExceptionExample {
+public class CatchByExceptionKindExample {
 	public static void main(String[] args) {
-		String data1 = null;
-		String data2 = null;
 		try {
-			data1 = args[0];
-			data2 = args[1];
-		} catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("실행 매개값의 수가 부족합니다.");
-			System.out.println("[실행 방법]");
-			System.out.println("java TryCatchFinallyRuntimeExceptionExample	num1	num2");
-			return;
-		}
-		
-		try {
+			String data1 = args[0];
+			String data2 = args[1];
 			int value1 = Integer.parseInt(data1);
 			int value2 = Integer.parseInt(data2);
 			int result = value1 + value2;
 			System.out.println(data1 + "+" + data2 + "=" + result);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("실행 매개값의 수가 부족합니다.");
+			System.out.println("[실행 방법]");
+			System.out.println("java CatchByExceptionKindExample	num1	num2");
 		} catch(NumberFormatException e) {
 			System.out.println("숫자로 변환할 수 없습니다.");
 		} finally {
