8e914aa3c28562268ec9f98a5378f7383cc0175f
박지연 <@192.168.219.103>
2021-01-28 17:20:28 +0900

    210128

--- lecture/src/com/urdomain/ch11/Key.java
+++ lecture/src/com/urdomain/ch11/Key.java
@@ -8,13 +8,7 @@ public class Key {
 	}
 	
 	@Override
-	public boolean equals(Object obj) {
-		if(obj instanceof Key) {
-			Key compareKey = (Key) obj;
-			if(this.number == compareKey.number) {
-				return true;
-			}
-		}
-		return false;
+	public int hashCode() {
+		return number;
 	}
 }
